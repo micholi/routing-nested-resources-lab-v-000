8f8bc8a577749f4e989cd0c7bbc5150cb005341c
Michelle Olivieri <michelleo72@gmail.com>
2018-06-09 01:16:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
       @song = Song.find_by_id(params[:id])
     elsif params[:artist_id] 
       @artist = Artist.find_by_id(params[:artist_id])
-      @song = @artist.songs.find_by_id(params[:id])
+      @song = Song.find_by_id(params[:id])
     elsif !@song
       flash[:alert] = "Song not found."
       redirect_to artist_songs_path(@artist) 
